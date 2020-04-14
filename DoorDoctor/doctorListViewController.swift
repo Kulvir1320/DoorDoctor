2586a514a28f643e482d295c16bede5c0b731b2b
Alisha <alisha27011997@gmail.com>
2020-04-14 00:17:00 -0400

    Added Doctor's List Layout

---
+++ DoorDoctor/doctorListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  doctorListViewController.swift
+//  DoorDoctor
+//
+//  Created by Alisha Thind on 2020-04-13.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import UIKit
+
+class doctorListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
