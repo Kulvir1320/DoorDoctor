16cf84273246230d5e90582d3538a78d2e425d7d
Amanpreet Kaur <aman021011@gmail.com>
2020-04-20 17:12:24 -0400

    [ak] Logout Action Correct

--- DoorDoctor/View Controllers/doctorListViewController.swift
+++ DoorDoctor/View Controllers/doctorListViewController.swift
@@ -171,11 +171,10 @@ class doctorListViewController: UIViewController,UITableViewDelegate, UITableVie
     @IBAction func LogOut(_ sender: UIBarButtonItem) {
     
         UserDefaults.standard.removeObject(forKey: "email")
-           if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginViewController") as? loginViewController {
-               if let navigator = navigationController {
-                   navigator.pushViewController(viewController, animated: true)
-               }
-           }
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainVC") as? ViewController
+
+                                 self.view.window?.rootViewController = vc
+                                 self.view.window?.makeKeyAndVisible()
 
     }
 }
